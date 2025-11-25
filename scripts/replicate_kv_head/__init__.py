c7af1f1ce23468e2bacbd3978b3763ed09ec3350
qraniumcitest <rmakar@qti.qualcomm.com>
2025-11-25 13:32:19 +0530

    Update __init__.py
    Signed-off-by: qraniumcitest <rmakar@qti.qualcomm.com>

--- scripts/replicate_kv_head/__init__.py
+++ scripts/replicate_kv_head/__init__.py
@@ -2,6 +2,5 @@
 #
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause
-#
 # -----------------------------------------------------------------------------
 
