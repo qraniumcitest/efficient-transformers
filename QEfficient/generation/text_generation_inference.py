f46959ff5965efb85450cc9fba0e6226c4dc10bd
Rishin Raj <rishinr@qti.qualcomm.com>
2025-12-17 11:52:56 +0530

    Update text_generation_inference.py
    Signed-off-by: Rishin Raj <rishinr@qti.qualcomm.com>

--- QEfficient/generation/text_generation_inference.py
+++ QEfficient/generation/text_generation_inference.py
@@ -7,9 +7,6 @@
 
 import json
 import os
-from collections import deque
-from dataclasses import dataclass
-from time import perf_counter
 from typing import Any, Dict, List, Optional, Tuple, Union
 
 import numpy as np
