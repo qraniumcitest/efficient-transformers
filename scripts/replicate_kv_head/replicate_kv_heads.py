0872015fee7b334017dcb9ea946fcd9dea174cb1
qraniumcitest <rmakar@qti.qualcomm.com>
2026-03-06 14:58:30 +0530

    Update replicate_kv_heads.py
    Signed-off-by: qraniumcitest <rmakar@qti.qualcomm.com>

--- scripts/replicate_kv_head/replicate_kv_heads.py
+++ scripts/replicate_kv_head/replicate_kv_heads.py
@@ -6,7 +6,7 @@
 # -----------------------------------------------------------------------------
 
 import argparse
-from typing import Optional
+from typing impor Optional
 
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
